93a6270dfd7fb938d49bfcf7934644593909116c
Kerley <kerley_sousa@live.com>
2019-09-30 23:12:12 -0300

    Inclusão de tentativas ao jogo

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,18 +4,28 @@ print("*********************************")
 
 numero_secreto = 42
 
-chute_str = input("Digite o seu número: ")
-print("Você digitou: ", chute_str)
-chute = int(chute_str)
+total_de_tentativas = 3
+rodada = 1
 
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+while (rodada <= total_de_tentativas):
 
-if (acertou):
-    print("Você acertou!")
-else:
-    if (maior):
-        print("Você errou! O seu chute foi maior que o número secreto.")
-    elif (menor):
-        print("Você errou! O seu chute foi menor que o número secreto.")
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou: ", chute_str)
+    chute = int(chute_str)
+
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if (acertou):
+        print("Você acertou!")
+    else:
+        if (maior):
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif (menor):
+            print("Você errou! O seu chute foi menor que o número secreto.")
+
+    rodada = rodada + 1
+
+print("Fim do jogo")
