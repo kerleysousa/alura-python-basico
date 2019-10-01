7293ee362c4991cc2eb010aac7488861f445c48a
Kerley <kerley_sousa@live.com>
2019-10-01 20:48:05 -0300

    Correção de bugs

--- adivinhacao.py
+++ adivinhacao.py
@@ -10,9 +10,12 @@ total_de_tentativas = 3
 for contador in range(1, total_de_tentativas+1):
 
     print("Tentativa {} de {}".format(contador, total_de_tentativas))
-    chute_str = input("Digite o seu número: ")
-    print("Você digitou: ", chute_str)
+    chute_str = input("Digite um número entre 1 e 100: ")
     chute = int(chute_str)
+
+    if (chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -20,6 +23,7 @@ for contador in range(1, total_de_tentativas+1):
 
     if (acertou):
         print("Você acertou!")
+        break
     else:
         if (maior):
             print("Você errou! O seu chute foi maior que o número secreto.")
