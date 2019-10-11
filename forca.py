382943db0280ee3384c4417416f89b121d20e9f2
Kerley <kerley_sousa@live.com>
2019-10-10 23:25:06 -0300

    Adicionada primeira versão do jogo Forca

--- forca.py
+++ forca.py
@@ -4,3 +4,15 @@ def jogar():
     print("***Bem vindo ao jogo de Forca!***")
     print("*********************************")
 
+    palavra_secreta = "python"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        print("Jogando...")
+
+    print("Fim de jogo")
+
+if (__name__ == "__main__ "):
+    jogar()
