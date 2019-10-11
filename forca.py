834aa4f0323e90e1d653168ae197f810e2d0b85e
Kerley <kerley_sousa@live.com>
2019-10-10 23:56:47 -0300

    Tratamento da entrada e comparação de dados

--- forca.py
+++ forca.py
@@ -4,13 +4,19 @@ def jogar():
     print("***Bem vindo ao jogo de Forca!***")
     print("*********************************")
 
-    palavra_secreta = "python"
+    palavra_secreta = "banana"
 
     enforcou = False
     acertou = False
 
-    while(not enforcou and not acertou):
-        print("Jogando...")
+    while not enforcou and not acertou:
+        chute = input("Qual a letra? ")
+        chute.strip()
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.lower() == letra.lower()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
 
     print("Fim de jogo")
 
