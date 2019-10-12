3de154bae448120286fdbc54bbf8266e7c8625a2
Kerley <kerley_sousa@live.com>
2019-10-11 23:56:18 -0300

    Adicionando listas ao projeto

--- forca.py
+++ forca.py
@@ -5,9 +5,12 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_","_","_","_","_","_"]
 
     enforcou = False
     acertou = False
+
+    print(letras_acertadas)
 
     while not enforcou and not acertou:
         chute = input("Qual a letra? ")
@@ -15,8 +18,10 @@ def jogar():
         index = 0
         for letra in palavra_secreta:
             if (chute.lower() == letra.lower()):
-                print("Encontrei a letra {} na posição {}".format(letra, index))
+                letras_acertadas[index] = chute
+                #print("Encontrei a letra {} na posição {}".format(letra, index))
             index = index + 1
+        print(letras_acertadas)
 
     print("Fim de jogo")
 
