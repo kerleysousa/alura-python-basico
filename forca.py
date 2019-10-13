09748f6993df4ecf91bd05f8a44dfb2fd4d06014
Kerley <kerley_sousa@live.com>
2019-10-12 21:09:14 -0300

    Adicionando parada ao jogo

--- forca.py
+++ forca.py
@@ -9,19 +9,32 @@ def jogar():
 
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
     while not enforcou and not acertou:
         chute = input("Qual a letra? ")
-        chute.strip()
+        chute.strip().lower()
         index = 0
-        for letra in palavra_secreta:
-            if (chute.lower() == letra.lower()):
-                letras_acertadas[index] = chute
-                #print("Encontrei a letra {} na posição {}".format(letra, index))
-            index = index + 1
+
+        if(chute in palavra_secreta):
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = chute
+                index += 1
+        else:
+            erros += 1
+            print("Você errou! Restam {} tentativas".format(6-erros))
+
+        enforcou = erros == 6
+        acertou = not "_" in letras_acertadas
         print(letras_acertadas)
+
+    if (acertou):
+        print("Você ganhou")
+    else:
+        print("Você perdeu")
 
     print("Fim de jogo")
 
